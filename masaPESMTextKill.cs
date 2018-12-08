4e3f1abd0fd24c46fa15e0eeb442e0214da6c308
maxstertko <33167611+maxstertko@users.noreply.github.com>
2018-12-08 16:18:17 -0500

    Add files via upload

---
+++ masaPESMTextKill.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class masaPESMTextKill : MonoBehaviour {
+
+    //CONTAINS TIMER AND SCORE GAMELOADER IS DISABLED
+
+    public float timer;
+    public bool isAlive;
+    public Text endText;
+    public Text startText;
+    public GameObject player;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        timer = 10;
+        isAlive = true;
+        endText.text = "";
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(player == null) {
+            isAlive = false;
+        }
+
+        StartCoroutine(RunGameDelay(2));
+       
+    }
+
+    IEnumerator RunGameDelay(float time) {
+        yield return new WaitForSeconds(time);
+        startText.text = "";
+        if (timer > 0 && isAlive == true) {
+            timer = timer - Time.deltaTime;
+        }
+        else if (timer <= 0 || isAlive == false) {
+            int wholeTime = 10 - (int)timer;
+
+            endText.text = "You did this for " + wholeTime + " Seconds!";
+
+            //GameLoader.AddScore(wholeTime);
+
+            StartCoroutine(ByeAfterDelay(2));
+
+        }
+    }
+
+    IEnumerator ByeAfterDelay(float time) {
+        yield return new WaitForSeconds(time);
+
+        //GameLoader.gameOn = false;
+    }
+
+   
+}
---
+++ masaPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class masaPlayerController : MonoBehaviour {
+
+    //gets boat position for the player to rotate around
+    public Transform ship;
+    private Vector3 shipPos;
+    private float rot_z;
+
+    
+
+    public float speed;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //Roates player to mimic being pulled by boat
+        shipPos = ship.transform.position - transform.position;
+        shipPos.Normalize();
+
+        rot_z = Mathf.Atan2(shipPos.y, shipPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+       
+        //Moves player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb.velocity = movement * speed;
+	}
+
+    
+}
